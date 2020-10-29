88a16a5c7b47af9b08eef6afb9f5c4d6411f0a15
Josiah Candler <josiahcandler@Josiahs-MacBook-Pro.local>
2020-10-29 15:33:39 -0600

    Crate Boot/MVC project

---
+++ Crescendo/src/main/java/com/skilldistillery/crescendo/CrescendoApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.crescendo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrescendoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrescendoApplication.class, args);
+	}
+
+}
---
+++ Crescendo/src/test/java/com/skilldistillery/crescendo/CrescendoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.crescendo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrescendoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
