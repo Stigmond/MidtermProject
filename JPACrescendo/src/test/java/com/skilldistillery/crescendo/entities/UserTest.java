eb59430b1ccd786336a4eb09e8e6fbd92c6d317e
Gray Morrow <tgrahammor@gmail.com>
2020-10-29 16:56:04 -0600

    User class added to persistence, mappings pass tests

--- JPACrescendo/src/test/java/com/skilldistillery/crescendo/entities/UserTest.java
+++ JPACrescendo/src/test/java/com/skilldistillery/crescendo/entities/UserTest.java
@@ -2,6 +2,10 @@ package com.skilldistillery.crescendo.entities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,26 +13,37 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class UserTest {
+	
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private User user;
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		emf = Persistence.createEntityManagerFactory("SocialApp");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		emf = null;
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		em = emf.createEntityManager();
+		user = em.find( User.class , 1 );
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		user = null;
+		em = null;
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testBindings() {
+		assertNotNull( user );
+		assertEquals( "admin" , user.getUsername() );
 	}
 
 }
