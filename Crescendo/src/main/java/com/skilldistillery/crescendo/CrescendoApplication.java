38088602f13f7bd78cb0238b45a4f61e46c0154a
Josiah Candler <josiahcandler@Josiahs-MacBook-Pro.local>
2020-11-05 13:30:27 -0700

    War file created

--- Crescendo/src/main/java/com/skilldistillery/crescendo/CrescendoApplication.java
+++ Crescendo/src/main/java/com/skilldistillery/crescendo/CrescendoApplication.java
@@ -2,9 +2,15 @@ package com.skilldistillery.crescendo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CrescendoApplication {
+public class CrescendoApplication extends SpringBootServletInitializer {
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(CrescendoApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrescendoApplication.class, args);
